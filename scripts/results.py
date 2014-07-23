50014beeffb60f89c7a722de3e8e9f1d5203e3fc
Rob Beagrie <rob@beagrie.com>
2014-07-23 14:59:04 +0100

    results.py should reflect the state at the beginning of the lesson

--- scripts/results.py
+++ scripts/results.py
@@ -1,20 +1,8 @@
-import sys
+import sys 
 import numpy as np
-from matplotlib import pyplot as plt
 
-usage_string = """
-Results.py plots the real overlap between two bed files
-versus the overlap when one of the results files is randomly
-shuffled.
-
-Usage: python random.py shuffled_results.txt real_results.txt
-"""
 
 def main():
-
-    if not len(sys.argv) == 3:
-        sys.exit(usage_string)
-
     script = sys.argv[0]
     shuffled_results_file = sys.argv[1]
     real_results_file = sys.argv[2]
@@ -32,11 +20,5 @@ def process(shuffled_results_file, real_results_file):
     print 'Shuffled data:'
     print shuffled_data
 
-    plt.hist(shuffled_data, color='black', label='Shuffled Data')
-    plt.axvline(real_data, color='red', ls='--', lw=2, label='Real Data')
-    plt.xlabel('Nucleotides overlap')
-    plt.ylabel('Number of shuffled results')
-    plt.legend()
-    plt.show()
-
 main()
+
